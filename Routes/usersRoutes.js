f3f65991ba0fa92af986c247d8955687812ebe51
henmo <henmor1987@gmail.com>
2019-10-08 01:56:13 +0300

    f

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -1,5 +1,8 @@
 var express = require('express');
 const clientController = require('../Controllers/clientsController.js');
+var mongo = require('mongodb').MongoClient;
+//Assert library (Perhaps overkill if you are writing production-level code)
+var assert = require('assert');
 
 var userRoutes = function () {
 
@@ -10,7 +13,23 @@ var userRoutes = function () {
     route.post('/removeClient',clientController.removeClient);
     
 
-    route.get('/ShowAllUsers', clientController.showAllUser );
+    route.get('/ShowAllUsers', function(req,res,next){
+        var resultArray = [];
+        mongo.connect(url, function(err, db){
+          assert.equal(null, err);
+          var cursor = db.collection('posts').find();
+          cursor.forEach(function(doc, err){
+            assert.equal(null, err);
+            resultArray.push(doc);
+          }, function(){
+            db.close();
+            //I have no index file to render, so I print the result to console
+            //Also send back the JSON string bare through the channel
+            console.log(resultArray);
+            res.send(resultArray);
+          });
+        });
+      });
 
 
 
